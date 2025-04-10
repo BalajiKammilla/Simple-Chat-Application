fcfa88b33eb2205239c2f1f19d7d9151efbda189
BalajiKammilla5 <149792462+BalajiKammilla5@users.noreply.github.com>
2025-04-10 16:56:22 +0530

    initial commit

---
+++ src/test/java/com/balu/Spring_boot_websocket/SpringBootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balu.Spring_boot_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
